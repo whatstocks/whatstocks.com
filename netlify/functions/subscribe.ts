a909690815230174f957f1b7c84ba89c979f9191
Mark Holt <mark.holt@distributed.vision>
2023-05-16 14:06:21 +0100

    get form

--- netlify/functions/subscribe.ts
+++ netlify/functions/subscribe.ts
@@ -1,10 +1,37 @@
 import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-    console.log("Event: ", event)
+    if (event.httpMethod=='POST') {
+        if (event.headers.origin=="") {  // TODO make this whatstocks.com
+            return {
+                statusCode: 403,
+            }
+        }
+
+        if (!event.headers["content-type"] || !event.headers["content-type"].startsWith('application/x-www-form-urlencoded')) {
+            return {
+                statusCode: 400,
+            }
+        }
+
+        if (!event.body) {
+            return {
+                statusCode: 400,
+            }
+        }
+
+        const form = new URLSearchParams(event.body)
+
+        console.log(form)
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Subscribe: Not Implemented" }),
+        }; 
+    }
+
     return {
-        statusCode: 500,
-        body: JSON.stringify({ message: "Subscribe: Not Implemented" }),
+        statusCode: 400
     }; 
 };
 
