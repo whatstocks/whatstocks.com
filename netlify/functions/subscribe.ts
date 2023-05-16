39e6448811df0f42e9cc1ffbaa58c310c8143f92
Mark Holt <mark.holt@distributed.vision>
2023-05-16 13:35:43 +0100

    log subscribe events

--- netlify/functions/subscribe.ts
+++ netlify/functions/subscribe.ts
@@ -1,6 +1,7 @@
 import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+    console.log("Event: ", event)
     return {
         statusCode: 500,
         body: JSON.stringify({ message: "Subscribe: Not Implemented" }),
